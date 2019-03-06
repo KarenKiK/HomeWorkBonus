734bcf791c13888e77d3ec3438e117d88cb7e3cc
student <kik_k-2@mail.ru>
2019-03-06 22:18:10 +0300

    Seller bonus search

--- src/main/java/ru/itpark/service/Main.java
+++ src/main/java/ru/itpark/service/Main.java
@@ -5,5 +5,6 @@ public class Main {
         BonusAmount service = new BonusAmount();
         int[] sales = {12_000, 8_000, 15_000, 9_000};
         int sum = service.calculate(sales);
+        System.out.println(sum);
     }
 }
